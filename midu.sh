c5bf639f54844052b4060c844fbea400fd067e29
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-10-14 17:00:20 +0200

    3535345

--- midu.sh
+++ midu.sh
@@ -8,11 +8,15 @@
 #https://learn.microsoft.com/en-us/azure/developer/javascript/how-to/with-visual-studio-code/clone-github-repository?tabs=create-repo-command-palette%2Cinitialize-repo-activity-bar%2Ccreate-branch-command-palette%2Ccommit-changes-command-palette%2Cpush-command-palette
 #https://git-scm.com/book/es/v2/Fundamentos-de-Git-Guardando-cambios-en-el-Repositorio
 
-if [ -d ]
+function Principal()
+{
+    echo "Hi"
+}
 
-if [ $# -lt 1 ] || [ $# -gt 2] #Si el numero de parametros no está entre 1 y 2
+if [ $# -lt 1 ] || [ $# -gt 2 ] #Si el numero de parametros no está entre 1 y 2
 then
-    echo "Error. Utilización: $0 [opciones] [camino1,camino2...]"
+    echo "Error. Utilización: $0 [opciones] [camino1,camino2...]"º
+    exit 1
 else
-    SumaLineaRecursiva $1 #y si sí, llamo a la función
+    Principal $1 $2 #y si sí, llamo a la función
 fi
