61097850e9f66ff6db7a196a455eb50a6b5ab975
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-10-14 11:21:44 +0200

    mensajito

--- midu.sh
+++ midu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Si estás leyendo esto quiere decir que el git funciona desde VSCODE - Manjaro
 if [ -d ]
 
 if [ $# -lt 1 ] || [ $# -gt 2] #Si el numero de parametros no está entre 1 y 2
