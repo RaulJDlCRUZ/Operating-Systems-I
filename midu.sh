ad956ce8f87811e4f9e40415ff324c9d9f721426
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-10-14 11:14:50 +0200

    111

--- midu.sh
+++ midu.sh
@@ -2,7 +2,7 @@
 
 if [ -d ]
 
-if [ $# -lt 1 ] || [ $# -gt 2] #Si el numero de parametros NO es 1
+if [ $# -lt 1 ] || [ $# -gt 2] #Si el numero de parametros no está entre 1 y 2
 then
     echo "Error. Utilización: $0 [opciones] [camino1,camino2...]"
 else
