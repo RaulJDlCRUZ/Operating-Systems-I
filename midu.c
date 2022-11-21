c77aa6903bd6dcc47fca244af008f6136d089e0e
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-11-22 00:28:17 +0100

    si

--- midu.c
+++ midu.c
@@ -10,112 +10,58 @@ int mostrarError(){
 }
 
 int main(int argc, char **argv){ //Voy a trabajar con la linea de ordenes, por lo que necesito un contador de argumentos y el puntero doble de argumentos
-    int opcion_exclude=0;
-    int opcion_s=0;
-    int opcion_d=0;
+    int flag_exclude=0;
+    int flag_s=0;
+    int flag_d=0;
+    int opcion=0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    // while(--argc){
-    //     switch (argc)
-    //     {
-    //     case '-s': case '-d': case '--exclude':
-    //         /*
-    //         if [ $CAMINOS ]; then #Error si ya se ha recogido antes uno o más caminos
-    //         mostrarError
-    //         fi
-    //         */
-    //         switch (argc)
-    //         {
-    //         case '-d': case '--exclude':
-    //             /*
-    //             if [ $CONTADOR -eq $# ]; then #Error si -d o --exclude es el ultimo param (y no tiene ningun valor detras)
-    //             mostrarError
-    //             fi
-    //             OPTION=$i
-    //             */
-    //             break;
-    //         case '-s':
-    //             opcion_s=1;
-    //             break;
-    //         }
-    //         break;
-    //     default:
-    //         switch (opcion)
-    //         {
-    //         case '-d':
-    //             /*
-    //             NIVELES=$(expr $i / 1)                                  #Paso a entero
-    //             if [ ! $NIVELES ] || [ $NIVELES -lt 0 ]; then
-    //             mostrarError
-    //             fi
-    //             OPTION_D=$NIVELES
-    //             */
-    //             break;
-    //         case '--exclude':
-    //             //OPTION_EXCL=$i 
-    //             break;
-    //         case ' ':
-    //             //CAMINOS+=("$i") #$i representa un camino/ruta la anyadimos al array/vector
-    //             break;
-    //         }
-    //         //OPTION=""
-    //         break;
-    //     }
-    // }
+    while(--argc>0){
+        switch (opcion=(*++argv))
+        {
+        case '-s': case '-d': case '--exclude':
+            /*
+            if [ $CAMINOS ]; then #Error si ya se ha recogido antes uno o más caminos
+            mostrarError
+            fi
+            */
+            switch (argc)
+            {
+            case '-d': case '--exclude':
+                /*
+                if [ $CONTADOR -eq $# ]; then #Error si -d o --exclude es el ultimo param (y no tiene ningun valor detras)
+                mostrarError
+                fi
+                OPTION=$i
+                */
+                break;
+            case '-s':
+                flag_s=1;
+                break;
+            }
+            break;
+        default:
+            switch (opcion)
+            {
+            case '-d':
+                /*
+                NIVELES=$(expr $i / 1)                                  #Paso a entero
+                if [ ! $NIVELES ] || [ $NIVELES -lt 0 ]; then
+                mostrarError
+                fi
+                OPTION_D=$NIVELES
+                */
+                break;
+            case '--exclude':
+                //OPTION_EXCL=$i 
+                break;
+            case ' ':
+                //CAMINOS+=("$i") #$i representa un camino/ruta la anyadimos al array/vector
+                break;
+            }
+            //OPTION=""
+            break;
+        }
+    }
 
 /*
 hola \0
@@ -123,14 +69,15 @@ habia\0
 int strcmp(PUNTERO1,PUNTERO)
 verdad=0
 mentira=resta direccion de primera o - primera a (pej: 67-65=2) DIFIERE DE 0
-
 */
 
 
-
-
-
 /*
+##################Comprobacion de errores#####################
+OPTION=""
+CONTADOR=1 #Se empieza en el arg 1
+OPTION_D=-99; OPTION_S=-99; OPTION_EXCL=-1 #Para que las variables con las opciones -d -s y --exclude se pasen siempre como param de la funcion
+for i in $@; do
 case $i in
     "-s" | "-d" | "--exclude")  ###CASO 1: OPCIONES
         if [ $CAMINOS ]; then #Error si ya se ha recogido antes uno o más caminos
@@ -169,6 +116,26 @@ _    *) #cualquier otra cosa:::: CASO DEFAULT
         ;;
     esac
     CONTADOR=$(expr $CONTADOR + 1)
-
+    #NO ES COMPATIBLE UTILIZAR LA OPCION -s CON LA OPCION -d EN MIDU PORQUE --summarize
+    #UTILIZA ÚNICAMENTE EL NIVEL 0 PARA EL CÓMPUTO DEL TAMAÑO (ÚNICO ARGUMENTO). CONSULTAR man du
+    if [ $OPTION_D != -99 ] && [ $OPTION_S != -99 ]; then
+        echo "La opción -s no es compatible con la opción -d"
+        mostrarError
+    fi
+done
+#ES AQUÍ CUANDO SE PROCEDE A INVOCAR A LA FUNCIÓN PARA CALCULAR EL TAMAÑO
+if [ ! $CAMINOS ]; then # Tratar cuando no se especifica un camino (".")
+    ComputoTam $OPTION_D $OPTION_S $OPTION_EXCL "."
+    echo "$tam_total ."
+    tam_total=0
+    echo "----------------------------------------------------------------------------------------------------------------------------------------------------" #Razón meramente estética
+else
+    for i in "${CAMINOS[@]}"; do
+        ComputoTam $OPTION_D $OPTION_S $OPTION_EXCL $i
+        echo "$tam_total $i"
+        tam_total=0
+        echo "----------------------------------------------------------------------------------------------------------------------------------------------------"
+    done
+fi
 */
 }
