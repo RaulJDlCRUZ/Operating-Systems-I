27e33d74d01010994c3a7235a852c7fc33446013
RaulJDlCRUZ <114936538+RaulJDlCRUZ@users.noreply.github.com>
2022-11-23 19:15:17 +0100

    mas update
    qw432t25g

--- midu.c
+++ midu.c
@@ -60,7 +60,7 @@ int main(int argc, char **argv){ //Voy a trabajar con la linea de ordenes, por l
             flag|=0001;
             }
             --argc;
-        }
+        } //!la llamada a recorrido de rutas es dentro del while!!!!!
         else{
             fprintf(stderr,"Has puesto otra cosa\n");
         }
