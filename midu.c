fe800b685c7a43b843f9dddc6669ada30fb6b2c3
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-11-29 19:41:11 +0100

    jhgfd

--- midu.c
+++ midu.c
@@ -8,11 +8,54 @@
 #define MAX_BUFFER 4096
 
 long ComputoTam(int opciones, int nivel, char *patron, const char *camino){ //?Buena idea si dejo los niveles y tal como parametros globales?
-    if ((opciones&04)>0)printf("-d con nivel %d\n",nivel);
-    if ((opciones&02)>0)printf("-s\n");
-    if ((opciones&01)>0)printf("--exclude y patron \"%s\"\n",patron);
+    // if ((opciones&04)>0)printf("-d con nivel %d\n",nivel);
+    // if ((opciones&02)>0)printf("-s\n");
+    // if ((opciones&01)>0)printf("--exclude y patron \"%s\"\n",patron);
     printf("%s\n",camino);
     return 255;
+    /*
+    local option_d=$1; local option_s=$2; local option_excl=$3; local camino=$4; local matching
+    #Almaceno en variables locales cada una de las opciones (han llegado como parámetros del control de errores) y otra variable que almacena si hay coincidencias con el patrón a excluir
+    if [ $option_excl != -1 ]; then
+        matching=$(echo $camino | rev | cut -d"/" -f1 | rev)
+        matching=$(echo $matching | grep -E $option_excl)
+    fi
+    #Si se ha activado --exclude, me quedo con el patrón y hago búsquedas hasta encontrar una coincidencia
+
+    if [ ! $matching ]; then #Si no hay coincidencias
+        if [ -f $camino ]; then #Si el camino es un arhivo regular...
+        local aux=$(wc -c < "$camino")
+        tam_total=$aux #El tamaño será el del archivo, el cual lo obtenemos de una variable local auxiliar. USAREMOS EN TODO MOMENTO WC -C PARA MOSTRAR BYTES
+        fi
+        if [ -d $camino ]; then #Si el camino es un directorio...
+            if [ $option_s != "-99" ]; then
+                option_d=0 #Dado a que --summarize (-s) implica un único tamaño para un único argumento, equivale a -d 0 (maxdepth = 0)
+            fi
+            local nodo #PUEDEN HABER SUBDIRECTORIOS: Creo una variable local nodo
+            for nodo in $camino/*; do #Recorro todos los subdirectorios con $camino/*, el asterisco es COMODÍN (CUALQUIER COSA => SUBDIRECTORIOS/ARCHIVOS REGULARES)
+                if [ -d $nodo ]; then #Si el nodo es un directorio...
+                    local temp=$tam_total #Guardamos el valor de tam_total en variable temporal
+                    tam_total=0 #Entonces reseteamos antes de la llamada a funcion recursiva
+                    if [ $option_d == "-99" ]; then
+                        ComputoTam $option_d $option_s $option_excl $nodo
+                    else
+                        ComputoTam $(expr $option_d - 1) $option_s $option_excl $nodo
+                    fi
+                    #Si -d está activada, llamamos a la función recursiva con los parámetros locales CON UN NIVEL INFERIOR. De lo contrario, el nivel es el mismo
+                    if [ $option_d == "-99" ] || [ $option_d -gt 0 ]; then
+                        echo "$tam_total $nodo"
+                    fi
+                    #Imprimimos si no tenemos $option_d activada, o si tenemos un valor positivo en dicha variable
+                    tam_total=$(expr $temp + $tam_total) #El tamaño total se acumula, sumándose el valor original (temp) y el nuevo (tam_total)
+                elif [ -f $nodo ]; then #Si el nodo es un archivo regular...
+                    local tam_archivo=$(wc -c < "$nodo")
+                    tam_total=$(expr $tam_total + $tam_archivo)
+                    #Similar a si el camino es un archivo regular, en una variable auxiliar guardo el resultado en bytes, y se lo sumo al tamaño total
+                fi
+            done #fin del for
+        fi
+    fi
+    */
 }
 
 int parametros(char *cadena){
