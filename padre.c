94a780b9dea1c9ade07168b1000773e18a63b8a0
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-12-26 16:58:00 +0100

    123123123

--- padre.c
+++ padre.c
@@ -1,6 +1,10 @@
 #include "stdio.h"
 
 int main(){
-    fprintf(stdout,"Fernando Alonso es el padre de los padres");
+    fprintf(stdout,"Aqui irá todo lo referente al control de errores. Lo que vendría siendo:\n Leer comentario xd");
+    /*
+    ! 1. Asegurarte de que todo lo que entre por la línea de órdenes (Excepto el propio nombre del programa) SEA UN ARCHIVO REGULAR
+    ! 2. Es por ello por lo que el número de argumentos (argc) tiene que ser EXPLÍCITAMENTE de al menos 2. (1 o menos NO SIRVE)
+    */
     return 0;
 }
