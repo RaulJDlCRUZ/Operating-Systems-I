6a69594966cf81b1d46c00e5dd9a800c6ecb68c6
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2022-12-28 00:28:43 +0100

    kfc

--- padre.c
+++ padre.c
@@ -1,3 +1,10 @@
+/*##############################AUTORES########################################*/
+/* - Pablo Blázquez Sánchez
+   - Raúl Jiménez de la Cruz
+        Grupo Martes
+*/
+/*#############################################################################*/
+/*   Práctica 3 - Laboratorio Sistemas Operativos I - Ingeniería Informática   */
     /*
     ! 1. Asegurarte de que todo lo que entre por la línea de órdenes (Excepto el propio nombre del programa) SEA UN ARCHIVO REGULAR
     ! 2. Es por ello por lo que el número de argumentos (argc) tiene que ser EXPLÍCITAMENTE de al menos 2. (1 o menos NO SIRVE)
@@ -5,6 +12,7 @@
     */
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,15 +20,11 @@
 #include <unistd.h>
 
 pid_t *pids;
-// int NUM_HIJOS; básicamente será argc - 1
-
-//! wc -l /ruta/...
-//* execlp ("wc", "wc", "-l", "/ruta/...", NULL)
+int NUM_HIJOS; //básicamente será argc - 1
 
 void matarProcesos(){
     for (int kt=0;kt<2;kt++){
         if(pids[kt]!=0){
-            //printf("Soy hijo %d\n",pids[kt]);
             if(kill(pids[kt],SIGTERM) /*Ejecuto el kill. Si el resultado es -1, error*/ == -1) fprintf(stderr,"Error al enviar la señal\n");
         }
     }
@@ -32,20 +36,30 @@ void manejadorSignal(){
     exit(EXIT_SUCCESS);
 }
 
-void padre(const char *args[]){
+void padre(int argc, char *argv[]){
     int it,jt,pid;
-    for (it=0;it<2/*NUMERO DE ARGUMENTOS -1 (nombre programa)*/;it++){
-    //fprintf(stdout,"%s\n",args[it]);
-    switch(pids[it]=fork()){
+    struct stat st;
+    for (it=1;it<(argc)/*NUMERO DE ARGUMENTOS -1 (nombre programa)*/;it++){
+    if( !stat(argv[it], &st) == 0 ){
+        fprintf(stderr, "Error en %s\n", argv[it]);
+        matarProcesos();
+        exit (EXIT_FAILURE);
+    }
+    if (!S_ISREG(st.st_mode)){
+        fprintf(stderr, "%s no es un archivo regular válido.\n", argv[it]);
+        matarProcesos();
+        exit (EXIT_FAILURE);
+    }
+    switch(pids[it-1]=fork()){
         case -1:
             fprintf(stderr,"Error al intentar crear proceso hijo\n");
             matarProcesos();
             exit(EXIT_FAILURE);
         case 0:
-            fprintf(stdout,"Inicio hijo %d con %s\n",getpid(),args[it]);
+            fprintf(stdout,"Inicio hijo %d con %s\n",getpid(),argv[it]);
             //fprintf(stdout,"Como soy hijo, duermo 2 segundos.\n");
             sleep(10);
-            execlp("wc","wc","-c",args[it],NULL);
+            execlp("wc","wc","-c",argv[it]/*++*/,NULL);
             fprintf(stderr,"Error al ejecutar el wc -c\n"); //Si no da problemas, esto no se hace
             matarProcesos();
             exit(EXIT_FAILURE);
@@ -53,9 +67,9 @@ void padre(const char *args[]){
         //     break;
         }
     }
-    for (it=0;it<2;it++){
+    for (it=0;it<(argc-1);it++){
         pid = wait(NULL);
-        for (jt=0;jt<2;jt++) if (pid == pids[jt]){
+        for (jt=0;jt<(argc-1);jt++) if (pid == pids[jt]){
             printf("Fin hijo %d\n",pid);
             pids[jt]=0;
         }
@@ -63,15 +77,18 @@ void padre(const char *args[]){
     }
 }
 
-int main(){
-    //?NUM_HIJOS=argc-1;
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        fprintf(stderr,"Error en número de argumentos. Modo de empleo: padre [<archivo>]*\n");
+        exit(EXIT_FAILURE);
+    }
+    NUM_HIJOS=argc-1;
     if(signal ( SIGINT , manejadorSignal ) == SIG_ERR) {
-        fprintf(stderr," Error en la manipulación de la señal \n" ) ;
+        fprintf(stderr,"Error en la manipulación de la señal.\n" );
         exit(EXIT_FAILURE) ;
     }
-    const char *args[] = {"/home/raul/Documentos/archivo.txt","/home/raul/textoprueba.txt"}; //Esto te lo ahorras al usar la linea de comandos argv[][] y argc
     //!Da algun problemilla asi tal cual si pones /home/<tu usuario>/*. Imagino que es porque hay directorios
-    pids = (pid_t*)malloc(2 /*numero de ficheros o HIJOS. argc-1*/ * sizeof(pid_t)); //Intrucción Memory Allocation. Con ello asigno de manera dinámica un espacio de memoria correcto
-    padre(args);
+    pids = (pid_t*)malloc(NUM_HIJOS /*numero de ficheros o HIJOS. argc-1*/ * sizeof(pid_t)); //Intrucción Memory Allocation. Con ello asigno de manera dinámica un espacio de memoria correcto
+    padre(argc, argv);
     return 0;
 }
