e7325dc7def3a412b21a26bcf5889e2ab182b265
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-12-28 01:10:46 +0100

    2123123123123123

--- padre.c
+++ padre.c
@@ -5,11 +5,6 @@
 */
 /*#############################################################################*/
 /*   Práctica 3 - Laboratorio Sistemas Operativos I - Ingeniería Informática   */
-    /*
-    ! 1. Asegurarte de que todo lo que entre por la línea de órdenes (Excepto el propio nombre del programa) SEA UN ARCHIVO REGULAR
-    ! 2. Es por ello por lo que el número de argumentos (argc) tiene que ser EXPLÍCITAMENTE de al menos 2. (1 o menos NO SIRVE)
-    * RECUERDA: padre [<archivo>]*
-    */
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -20,7 +15,7 @@
 #include <unistd.h>
 
 pid_t *pids;
-int NUM_HIJOS; //básicamente será argc - 1
+int NUM_HIJOS;
 
 void matarProcesos(){
     for (int kt=0;kt<2;kt++){
@@ -32,48 +27,44 @@ void matarProcesos(){
 
 void manejadorSignal(){
     matarProcesos();
-    fprintf(stdout,"Padre finaliza por señal de interrupcion\n");
+    fprintf(stdout,"[Padre finaliza por señal de interrupcion]\n");
     exit(EXIT_SUCCESS);
 }
 
 void padre(int argc, char *argv[]){
     int it,jt,pid;
     struct stat st;
-    for (it=1;it<(argc)/*NUMERO DE ARGUMENTOS -1 (nombre programa)*/;it++){
+    for (it=1;it<argc;it++){ //Me salto el nombre del programa, por ello empiezo en el 1. Entonces es hasta el número de argumentos
     if( !stat(argv[it], &st) == 0 ){
-        fprintf(stderr, "Error en %s\n", argv[it]);
+        fprintf(stderr, "Error en %s\n", argv[it]); //Si no es un fichero o un directorio error
         matarProcesos();
         exit (EXIT_FAILURE);
     }
     if (!S_ISREG(st.st_mode)){
-        fprintf(stderr, "%s no es un archivo regular válido.\n", argv[it]);
+        fprintf(stderr, "%s no es un archivo regular válido.\n", argv[it]); //Y si no es un directorio, no nos sirve
         matarProcesos();
         exit (EXIT_FAILURE);
     }
-    switch(pids[it-1]=fork()){
+    switch(pids[it-1]=fork()){ //Usamos un -1 para el puntero de pids para compensar el inicio en 1 del bucle for
         case -1:
             fprintf(stderr,"Error al intentar crear proceso hijo\n");
             matarProcesos();
             exit(EXIT_FAILURE);
         case 0:
             fprintf(stdout,"Inicio hijo %d con %s\n",getpid(),argv[it]);
-            //fprintf(stdout,"Como soy hijo, duermo 2 segundos.\n");
             sleep(10);
-            execlp("wc","wc","-c",argv[it]/*++*/,NULL);
-            fprintf(stderr,"Error al ejecutar el wc -c\n"); //Si no da problemas, esto no se hace
+            execlp("wc","wc","-c",argv[it],NULL);
+            fprintf(stderr,"Error al ejecutar el wc -c\n"); //Si no da problemas, esto (y lo siguiente) no se hace
             matarProcesos();
             exit(EXIT_FAILURE);
-        // default:
-        //     break;
         }
     }
-    for (it=0;it<(argc-1);it++){
+    for (it=0;it<NUM_HIJOS;it++){
         pid = wait(NULL);
-        for (jt=0;jt<(argc-1);jt++) if (pid == pids[jt]){
+        for (jt=0;jt<NUM_HIJOS;jt++) if (pid == pids[jt]){
             printf("Fin hijo %d\n",pid);
             pids[jt]=0;
         }
-        //printf("%d y actual pid almacenado: %d\n",pid,pids[it]);
     }
 }
 
@@ -85,9 +76,8 @@ int main(int argc, char *argv[]){
     NUM_HIJOS=argc-1;
     if(signal ( SIGINT , manejadorSignal ) == SIG_ERR) {
         fprintf(stderr,"Error en la manipulación de la señal.\n" );
-        exit(EXIT_FAILURE) ;
+        exit(EXIT_FAILURE);
     }
-    //!Da algun problemilla asi tal cual si pones /home/<tu usuario>/*. Imagino que es porque hay directorios
     pids = (pid_t*)malloc(NUM_HIJOS /*numero de ficheros o HIJOS. argc-1*/ * sizeof(pid_t)); //Intrucción Memory Allocation. Con ello asigno de manera dinámica un espacio de memoria correcto
     padre(argc, argv);
     return 0;
