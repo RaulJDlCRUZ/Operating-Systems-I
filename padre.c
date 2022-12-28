16dc5712264c3bfad77451529fbfe4e3f02e35eb
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2022-12-28 03:13:07 +0100

    esta mejor

--- padre.c
+++ padre.c
@@ -6,8 +6,8 @@
 /*#############################################################################*/
 /*   Práctica 3 - Laboratorio Sistemas Operativos I - Ingeniería Informática   */
 
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,60 +15,74 @@
 #include <unistd.h>
 
 pid_t *pids;
-int NUM_HIJOS;
+int NUM_HIJOS = 0;
+
+void inicializaracero(pid_t *pids){
+    for(int i = 0; i < NUM_HIJOS; i++) pids[i] = 0;
+}
 
 void matarProcesos(){
-    for (int kt=0;kt<2;kt++){
+    for (int kt = 0; kt < NUM_HIJOS; kt++){
         if(pids[kt]!=0){
-            if(kill(pids[kt],SIGTERM) /*Ejecuto el kill. Si el resultado es -1, error*/ == -1) fprintf(stderr,"Error al enviar la señal\n");
+            if(kill(pids[kt],SIGTERM)/*Ejecuto el kill. Si el resultado es -1, error*/ == -1){
+                fprintf(stderr,"Error al enviar la señal\n");
+                free(pids);
+                exit(EXIT_FAILURE);
+            }
+            pids[kt]=0; //Si no da error, simplemente lo guardamos como cero, hemos matado el proceso hijo, ya no almacenaremos su PID
         }
     }
 }
 
-void manejadorSignal(){
+void manejadorSignal(int signal){
     matarProcesos();
     fprintf(stdout,"[Padre finaliza por señal de interrupcion]\n");
+    free(pids);
     exit(EXIT_SUCCESS);
 }
 
+void cerrarPadreporError(){ //Cuando se está ejecutando el método padre tenemos un error, declaramos este método para evitar repetir instrucciones. Simplemente se llama al método de matar los procesos hijos. liberación de memoria dinámica y salir con código de error.
+    matarProcesos();
+    free(pids);
+    exit (EXIT_FAILURE);
+}
+
 void padre(int argc, char *argv[]){
-    int it,jt,pid;
+    int it,jt;
+    pid_t pid;
     struct stat st;
-    for (it=1;it<argc;it++){ //Me salto el nombre del programa, por ello empiezo en el 1. Entonces es hasta el número de argumentos
+
+    for (it = 1; it < argc; it++){ //Me salto el nombre del programa, por ello empiezo en el 1. Entonces es hasta el número de argumentos
     if( !stat(argv[it], &st) == 0 ){
-        fprintf(stderr, "Error en %s\n", argv[it]); //Si no es un fichero o un directorio error
-        matarProcesos();
-        exit (EXIT_FAILURE);
+        fprintf(stderr, "Error en %s. Modo de empleo: padre [<archivo>]*\n", argv[it]); //Si no es un fichero o un directorio error
+        cerrarPadreporError();
     }
     if (!S_ISREG(st.st_mode)){
-        fprintf(stderr, "%s no es un archivo regular válido.\n", argv[it]); //Y si no es un directorio, no nos sirve
-        matarProcesos();
-        exit (EXIT_FAILURE);
+        fprintf(stderr, "%s no es un archivo regular válido. Modo de empleo: padre [<archivo>]*\n", argv[it]); //Y si no es un directorio, no nos sirve
+        cerrarPadreporError();
     }
     switch(pids[it-1]=fork()){ //Usamos un -1 para el puntero de pids para compensar el inicio en 1 del bucle for
         case -1:
             fprintf(stderr,"Error al intentar crear proceso hijo\n");
-            matarProcesos();
-            exit(EXIT_FAILURE);
+            cerrarPadreporError();
         case 0:
             fprintf(stdout,"Inicio hijo %d con %s\n",getpid(),argv[it]);
             sleep(10);
             execlp("wc","wc","-c",argv[it],NULL);
             fprintf(stderr,"Error al ejecutar el wc -c\n"); //Si no da problemas, esto (y lo siguiente) no se hace
-            matarProcesos();
-            exit(EXIT_FAILURE);
+            cerrarPadreporError();
         }
     }
-    for (it=0;it<NUM_HIJOS;it++){
+    for (it = 0; it < NUM_HIJOS; it++){
         pid = wait(NULL);
-        for (jt=0;jt<NUM_HIJOS;jt++) if (pid == pids[jt]){
+        for (jt = 0; jt < NUM_HIJOS; jt++) if (pid == pids[jt]){
             printf("Fin hijo %d\n",pid);
             pids[jt]=0;
         }
     }
 }
 
-int main(int argc, char *argv[]){
+void main(int argc, char *argv[]){
     if(argc <= 1){
         fprintf(stderr,"Error en número de argumentos. Modo de empleo: padre [<archivo>]*\n");
         exit(EXIT_FAILURE);
@@ -78,7 +92,9 @@ int main(int argc, char *argv[]){
         fprintf(stderr,"Error en la manipulación de la señal.\n" );
         exit(EXIT_FAILURE);
     }
-    pids = (pid_t*)malloc(NUM_HIJOS /*numero de ficheros o HIJOS. argc-1*/ * sizeof(pid_t)); //Intrucción Memory Allocation. Con ello asigno de manera dinámica un espacio de memoria correcto
-    padre(argc, argv);
-    return 0;
+    pids = (pid_t*)malloc(NUM_HIJOS /*numero de ficheros o HIJOS. argc-1*/ * sizeof(pid_t)); //Intrucción Memory Allocation. Con ello asigno de manera dinámica un espacio de memoria correcto Sin embargo, como se puede utilizar cualquier espacio de memoria, vamos a inicializar a cero todo el contenido del puntero:
+    inicializaracero(pids);
+    padre(argc, argv); //Una vez hechos los preparativos, saltamos a la función de creación de procesos
+    free(pids); //Al usar memoria dinamica, tengo que liberar el espacio utilizado durante la ejecucion
+    exit(EXIT_SUCCESS);
 }
